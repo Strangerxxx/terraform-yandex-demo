be9679fcf6d0b6ec96b8e738dbc9ce5756d4cb51
Ivan <ivan-vetkasov@yandex-team.ru>
2019-01-29 16:24:11 +0300

    RKE binary architecture handling

--- scripts/rke.sh
+++ scripts/rke.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 if [[ ! -e bin/rke ]]; then
     mkdir -p bin
-    curl -L https://github.com/rancher/rke/releases/download/v0.1.13/rke -o bin/rke
+    arch=$(uname -s)
+    if [[ $arch = "Darwin" ]]; then
+        curl -L https://github.com/rancher/rke/releases/download/v0.1.13/rke_darwin-amd64 -o bin/rke
+    elif [[ "$arch" = "Linux" ]]; then
+        curl -L https://github.com/rancher/rke/releases/download/v0.1.13/rke_linux-amd64 -o bin/rke
+    else
+        #curl -L https://github.com/rancher/rke/releases/download/v0.1.13/rke -o bin/rke
+        echo "Platform is not supported: $arch"
+        exit 1
+    fi
     chmod +x bin/rke
 fi
 if [[ ! -z $1 ]]; then
